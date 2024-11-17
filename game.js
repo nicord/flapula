28424b6ce19fe0569e3044607628aef674a1369d
Nicolas Robert Dehault <nrobertdehault@salesforce.com>
2024-11-17 18:39:37 +0100

    wip

--- game.js
+++ game.js
@@ -1,18 +1,21 @@
 var myGameArea = {
   canvas: document.createElement("canvas"),
   start: function () {
-    this.canvas.width = 480;
-    this.canvas.height = 270;
+    this.canvas.width = 1000;
+    this.canvas.height = 500;
     this.context = this.canvas.getContext("2d");
     document.body.appendChild(this.canvas);
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  clear: function () {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   },
 };
 
 var myGamePiece;
 
 function startGame() {
-  myGameArea.start();
-  myGamePiece = new component(30, 30, "red", 10, 120);
+  myGamePiece = new component(30, 30, "#900c0c", 10, 225);
 }
 
 function component(width, height, color, x, y) {
@@ -20,7 +23,15 @@ function component(width, height, color, x, y) {
   this.height = height;
   this.x = x;
   this.y = y;
-  ctx = myGameArea.context;
-  ctx.fillStyle = color;
-  ctx.fillRect(this.x, this.y, this.width, this.height);
+  this.update = function () {
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  };
+}
+
+function updateGameArea() {
+  myGameArea.clear();
+  myGamePiece.x += 1;
+  myGamePiece.update();
 }
